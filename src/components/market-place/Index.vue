ffe4393cb96336a5b7a5932003ddb7944b1149e9
hasan <hasanul@ackosoft.com>
2020-07-25 15:45:05 +0600

    wishlist and 2 other page converted to vue

--- src/components/market-place/Index.vue
+++ src/components/market-place/Index.vue
@@ -10,17 +10,17 @@
                                 <a href="" class="text-white float-right">View All</a>
                             </div>
                             <div class="card-body category-list">
-                                <div v-for="(category, index) in categories" v-bind:key="index">
-                                    <a href="Javascript:void(0)" class="active" v-if="category.children.length === 0">
+                                <template v-for="(category, index) in categories" >
+                                    <a href="Javascript:void(0)" class="active" v-if="category.children.length === 0" v-bind:key="index">
                                         <i class="fas fa-female"></i>
                                         {{category.name}}
                                     </a>
 
 
-                                    <a href="Javascript:void(0)" class="dropdown-toggle" role="button" :id="index" v-else
+                                    <a href="Javascript:void(0)" class="dropdown-toggle" role="button" :id="index" v-bind:key="index" v-else
                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
                                             class="fas fa-tv"></i> {{category.name}}</a>
-                                    <div class="dropdown-menu sho" :aria-labelledby="index">
+                                    <div class="dropdown-menu sho" :aria-labelledby="index" v-bind:key="index">
                                         <div class="row">
                                             <div class="col-4" v-for="(children, index2) in category.children"
                                                  v-bind:key="index2">
@@ -36,7 +36,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </template>
                             </div>
 
                             <div class="card-body category-list">
